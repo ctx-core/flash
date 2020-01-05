ffd6e4293836d92fc31cfd127282d15db47b76f8
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:03:34 -0500

    feat: store: b__ factories
    ∋ "@ctx-core/dom": "^5.10.44"

--- store.d.ts
+++ store.d.ts
@@ -1,12 +1,21 @@
 /// <reference types="node" />
-export declare const __queue__flash: import("svelte/store").Writable<any[]>;
+import { Writable } from 'svelte/store';
+export interface Writable__queue__flash extends Writable<any[]> {
+    add__flash: (ctx__flash: any) => void;
+    shift__flash: () => void;
+    add__message__flash: (message__flash: any, rest?: any) => void;
+    add__error__flash: (error__flash: any, rest?: any) => void;
+    cancel__expire__flash: () => void;
+}
+export declare const b__queue__flash: (ctx?: any, opts?: any) => Writable__queue__flash;
+export declare const __queue__flash: Writable__queue__flash;
+export declare const add__flash: (ctx__flash: any) => void, shift__flash: () => void, add__message__flash: (message__flash: any, rest?: any) => void, add__error__flash: (error__flash: any, rest?: any) => void, cancel__expire__flash: () => void;
 export declare let timeout__flash: number;
+export declare const b__flash: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __flash: import("svelte/store").Readable<any>;
+export declare const b__message__flash: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __message__flash: import("svelte/store").Readable<any>;
+export declare const b__error__flash: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __error__flash: import("svelte/store").Readable<any>;
+export declare const b__id__timeout__expire__flash: (ctx?: any, opts?: any) => import("svelte/store").Readable<NodeJS.Timeout>;
 export declare const __id__timeout__expire__flash: import("svelte/store").Readable<NodeJS.Timeout>;
-export declare function add__flash(ctx__flash: any): void;
-export declare function shift__flash(): void;
-export declare function add__message__flash(message__flash: any, rest?: any): void;
-export declare function add__error__flash(error__flash: any, rest?: any): void;
-export declare function cancel__expire__flash(): void;
--- store.ts
+++ store.ts
@@ -1,54 +1,90 @@
-import { writable, derived, get } from 'svelte/store'
+import { writable, derived, get, Writable } from 'svelte/store'
 import { subscribe } from '@ctx-core/store'
 // @ts-ignore
 import { subscribe__debug } from '@ctx-core/store'
-import { clone } from '@ctx-core/object'
+import { _b, assign, clone } from '@ctx-core/object'
 import { tap, _andand, noop } from '@ctx-core/function'
-export const __queue__flash = writable([])
+import { _has__dom } from '@ctx-core/dom'
+export interface Writable__queue__flash extends Writable<any[]> {
+	add__flash:(ctx__flash:any)=>void
+	shift__flash:()=>void
+	add__message__flash:(message__flash:any, rest?:any)=>void
+	add__error__flash:(error__flash:any, rest?:any)=>void
+	cancel__expire__flash:()=>void
+}
+export const b__queue__flash = _b<Writable__queue__flash>('__queue__flash', ctx=>{
+	const __queue__flash = writable([])
+	return assign(__queue__flash, {
+		add__flash,
+		shift__flash,
+		add__message__flash,
+		add__error__flash,
+		cancel__expire__flash,
+	})
+	function mutate__flash(fn) {
+		__queue__flash.update(
+			queue__flash=>
+				tap(queue__flash.slice(),
+					queue__flash__=>
+						fn(queue__flash__)
+				)
+		)
+	}
+	function add__flash(ctx__flash) {
+		mutate__flash(
+			queue__flash=>
+				queue__flash.push(ctx__flash))
+	}
+	function shift__flash() {
+		mutate__flash(
+			queue__flash=>
+				queue__flash.shift())
+	}
+	function add__message__flash(message__flash, rest?) {
+		add__flash(clone(rest, {
+			message__flash,
+		}))
+	}
+	function add__error__flash(error__flash, rest?) {
+		add__flash(clone(rest, {
+			error__flash,
+		}))
+	}
+	function cancel__expire__flash() {
+		window.clearTimeout(
+			get(b__id__timeout__expire__flash(ctx)))
+	}
+})
+export const __queue__flash = b__queue__flash()
+export const {
+	add__flash,
+	shift__flash,
+	add__message__flash,
+	add__error__flash,
+	cancel__expire__flash,
+} = __queue__flash
 export let timeout__flash = 10000
-function mutate__flash(fn) {
-	__queue__flash.update(
-		queue__flash =>
-			tap(queue__flash.slice(),
-				queue__flash__ =>
-					fn(queue__flash__)
-			)
-	)
-}
-export const __flash =
-	derived(__queue__flash, _andand(0))
-export const __message__flash =
-	derived(__flash, _andand('message__flash'))
-export const __error__flash =
-	derived(__flash, _andand('error__flash'))
-export const __id__timeout__expire__flash =
-	derived(__flash,
-		() =>
-			setTimeout(
-				shift__flash,
-				timeout__flash)
-	)
-subscribe(__id__timeout__expire__flash, noop)
-export function add__flash(ctx__flash) {
-	mutate__flash(
-		queue__flash =>
-			queue__flash.push(ctx__flash))
-}
-export function shift__flash() {
-	mutate__flash(
-		queue__flash =>
-			queue__flash.shift())
-}
-export function add__message__flash(message__flash, rest?) {
-	add__flash(clone(rest, {
-		message__flash,
-	}))
-}
-export function add__error__flash(error__flash, rest?) {
-	add__flash(clone(rest, {
-		error__flash,
-	}))
-}
-export function cancel__expire__flash() {
-	window.clearTimeout(get(__id__timeout__expire__flash))
-}
+export const b__flash = _b('__flash', ctx=>
+	derived(b__queue__flash(ctx), _andand(0)))
+export const __flash = b__flash()
+export const b__message__flash = _b('__message__flash', ctx=>
+	derived(b__flash(ctx), _andand('message__flash')))
+export const __message__flash = b__message__flash()
+export const b__error__flash = _b('__error__flash', ctx=>
+	derived(b__flash(ctx), _andand('error__flash')))
+export const __error__flash = b__error__flash()
+export const b__id__timeout__expire__flash = _b('__id__timeout__expire__flash', ctx=>{
+	const __id__timeout__expire__flash =
+		derived(
+			b__flash(ctx),
+			()=>
+				setTimeout(
+					shift__flash,
+					timeout__flash)
+		)
+	if (_has__dom()) {
+		subscribe(__id__timeout__expire__flash, noop)
+	}
+	return __id__timeout__expire__flash
+})
+export const __id__timeout__expire__flash = b__id__timeout__expire__flash()
