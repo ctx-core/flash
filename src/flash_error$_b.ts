f3289b36841fd36cdde52116410c7e201d1b5baf
Brian Takita <brian.takita@gmail.com>
2021-11-16 21:22:55 -0600

    formatting

--- src/flash_error$_b.ts
+++ src/flash_error$_b.ts
@@ -1,7 +1,7 @@
 import { B, be_ } from '@ctx-core/object'
 import { derived$, Readable$ } from '@ctx-core/store'
-import { flash$_b } from './flash$_b.js'
 import type { flash_Ctx } from './flash_Ctx.js'
+import { flash$_b } from './flash$_b.js'
 const key = 'flash_error$'
 export const flash_error$_b:B<flash_Ctx, typeof key> = be_(key, ctx=>
 	derived$(flash$_b(ctx), $flash=>$flash?.flash_error))
--- src/flash_expire_timeout_id$_b.ts
+++ src/flash_expire_timeout_id$_b.ts
@@ -1,9 +1,9 @@
 import { B, be_ } from '@ctx-core/object'
 import { derived$, Readable$ } from '@ctx-core/store'
 import { flash$_b } from './flash$_b.js'
-import { flash_timeout } from './flash_timeout.js'
-import { flash_queue$_b } from './flash_queue$_b.js'
 import type { flash_Ctx } from './flash_Ctx.js'
+import { flash_queue$_b } from './flash_queue$_b.js'
+import { flash_timeout } from './flash_timeout.js'
 const key = 'flash_expire_timeout_id$'
 export const flash_expire_timeout_id$_b:B<flash_Ctx, typeof key> = be_(key, ctx=>{
 	const flash_queue = flash_queue$_b(ctx)
